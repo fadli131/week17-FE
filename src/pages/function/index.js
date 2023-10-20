12d81b79ba5388949477b1d4f7f14aae43bf9764
fadli131 <fadliaryadinata011@gmail.com>
2023-10-21 02:04:55 +0700

    first

--- src/pages/function/index.js
+++ src/pages/function/index.js
@@ -1,6 +1,6 @@
 import Swal from "sweetalert2"
 
-export const API_BASE_URL = 'https://tame-handkerchief-cod.cyclic.app'
+export const API_BASE_URL = 'tame-handkerchief-codCyclicDB'
 
 export const getHeaders = () => {
     const token = localStorage.getItem('token');
